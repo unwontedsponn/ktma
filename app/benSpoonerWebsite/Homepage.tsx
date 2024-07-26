cf2f0e58710569d902ebfb5af0a030a1ca03718f
unwontedsponn <benpaulspooner@gmail.com>
2024-07-26 08:34:43 +0100

    Updated Homepage for mobile by making text hidden and smaller across sizes

--- app/benSpoonerWebsite/Homepage.tsx
+++ app/benSpoonerWebsite/Homepage.tsx
@@ -14,13 +14,13 @@ const Homepage: React.FC = () => {
       
         {/* Left Column */}
         <div id="title-div" className="flex flex-col">   
-              <SlideFadeIn direction="down" className="text-sm pl-36 color-green font-gopher-mono-semi tracking-mediumphomepage"><p>I like to get things done</p></SlideFadeIn>         
+              <SlideFadeIn direction="down" className="hidden md:block text-sm pl-36 color-green font-gopher-mono-semi tracking-mediumphomepage"><p>I like to get things done</p></SlideFadeIn>         
 
-              <SlideFadeIn direction="left" className="font-gopher-mono-semi color-blue leading-none text-7xl md:text-10xl mb-0"><p className="opacity-40">Hello,</p></SlideFadeIn>
+              <SlideFadeIn direction="left" className="font-gopher-mono-semi color-blue leading-none text-9xl sm:text-10xl mb-0"><p className="opacity-40">Hello</p></SlideFadeIn>
 
-              <SlideFadeIn direction="right" className="text-2xl md:text-4xl max-w-2xl color-dark-blue font-gopher-mono underline tracking-largep text-decoration-color pl-24"><p>and I like making... </p><RotatingWords words={words} /></SlideFadeIn>
+              <SlideFadeIn direction="right" className="hidden md:block text-4xl max-w-2xl color-dark-blue font-gopher-mono underline tracking-largep text-decoration-color pl-24"><p>and I like making... </p><RotatingWords words={words} /></SlideFadeIn>
 
-              <SlideFadeIn direction="left" className="font-gopher-mono-semi color-blue leading-none text-8xl md:text-11xl pb-0"><p className="opacity-40">I&apos;m Ben</p></SlideFadeIn>
+              <SlideFadeIn direction="left" className="font-gopher-mono-semi color-blue leading-none hidden md:block text-11xl pb-0"><p className="opacity-40">I&apos;m Ben</p></SlideFadeIn>
 
               <SlideFadeIn direction="up" className="hidden md:block pl-28 text-dark-500 font-gopher-mono tracking-smallphomepage text-xs max-w-2xl">
                 {`I thrive on bringing ideas to life. From creating books and games to websites and musical pieces, I'm fueled by a passion for creative endeavors, and `}
