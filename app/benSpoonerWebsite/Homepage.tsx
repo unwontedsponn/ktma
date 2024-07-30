0a09cbeac123adaf562320d1ea280732f955f844
unwontedsponn <benpaulspooner@gmail.com>
2024-07-30 13:52:50 +0100

    restyled homepage for mobile with image only no text

--- app/benSpoonerWebsite/Homepage.tsx
+++ app/benSpoonerWebsite/Homepage.tsx
@@ -16,15 +16,29 @@ const Homepage: React.FC = () => {
         <div id="title-div" className="flex flex-col">   
           <SlideFadeIn direction="down" className="hidden md:block text-sm md:pl-36 color-green font-gopher-mono-semi tracking-mediumphomepage"><p>I like to get things done</p></SlideFadeIn>         
 
-          <SlideFadeIn direction="left" className="font-gopher-mono-semi color-blue leading-none text-20vw md:text-10xl"><p className="opacity-40">Hello,</p></SlideFadeIn>
+          <SlideFadeIn direction="left" className="font-gopher-mono-semi color-blue leading-none hidden md:block text-10xl"><p className="opacity-40">Hello,</p></SlideFadeIn>
 
           <SlideFadeIn direction="right" className="hidden md:block text-4xl max-w-2xl color-dark-blue font-gopher-mono underline tracking-largep text-decoration-color md:pl-24"><p>and I like making... </p><RotatingWords words={words} /></SlideFadeIn>
 
-          <SlideFadeIn direction="left" className="font-gopher-mono-semi color-blue leading-none text-20vw md:text-11xl pb-0"><p className="opacity-40">I&apos;m Ben</p></SlideFadeIn>
+          <SlideFadeIn direction="left" className="font-gopher-mono-semi color-blue leading-none hidden md:block text-11xl pb-0"><p className="opacity-40">I&apos;m Ben</p></SlideFadeIn>
 
           <SlideFadeIn direction="up" className="hidden md:block pl-28 text-dark-500 font-gopher-mono tracking-smallphomepage text-xs max-w-2xl">
             {`I thrive on bringing ideas to life. From creating books and games to websites and musical pieces, I'm fueled by a passion for creative endeavors, and `}
             <span className="italic">getting things done</span>
+          </SlideFadeIn>
+
+          {/* Image for Mobile */}
+          <SlideFadeIn className="md:hidden border-3 border-thick-border-gray">
+            <div className="w-full max-w-sm mx-auto">
+              <Image
+                src='/images/homepage-pic.jpeg'
+                alt="homepage picture"                
+                width={500}
+                height={500}
+                priority
+                className="w-full h-auto"
+              />
+            </div>
           </SlideFadeIn>
         </div>
 
