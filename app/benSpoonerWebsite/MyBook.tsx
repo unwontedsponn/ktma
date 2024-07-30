4500a029ec6d52de595494abdcb3b80d4830ec21
unwontedsponn <benpaulspooner@gmail.com>
2024-07-30 13:23:25 +0100

    Updated myBook for mobile

--- app/benSpoonerWebsite/MyBook.tsx
+++ app/benSpoonerWebsite/MyBook.tsx
@@ -12,14 +12,14 @@ const MyBook: React.FC = () => {
         <div className="flex flex-row justify-center gap-x-8 px-32 items-center h-screen overflow-hidden">
         
           {/* Left Column */}
-          <div className="sm:flex flex-col hidden">       
+          <div className="flex-col">       
             <SlideFadeIn direction="left" className="hidden md:block color-blue font-gopher-mono-semi leading-none text-11xl"><h1 className="opacity-40">myBook</h1></SlideFadeIn>
 
-            <SlideFadeIn direction="right" className="color-dark-blue font-gopher-mono underline text-decoration-color text-4xl md:pl-32 tracking-largep whitespace-nowrap"><p className="">Beginner To Composer...</p></SlideFadeIn>
+            <SlideFadeIn direction="right" className="color-dark-blue font-gopher-mono underline text-decoration-color text-4vw md:text-4xl md:pl-32 tracking-largep whitespace-nowrap"><p className="">Beginner To Composer...</p></SlideFadeIn>
 
-            <SlideFadeIn direction="right" className="color-dark-blue font-gopher-mono underline text-decoration-color tracking-largep text-2xl md:pl-32"><p>in 14 days</p></SlideFadeIn>
+            <SlideFadeIn direction="right" className="color-dark-blue font-gopher-mono underline text-decoration-color tracking-largep text-3vw md:text-2xl md:pl-32"><p>in 14 days</p></SlideFadeIn>
 
-            <SlideFadeIn direction="left" className=" text-xs color-dark font-gopher-mono pt-4">
+            <SlideFadeIn direction="left" className="text-2vw md:text-xs color-dark font-gopher-mono pt-4">
               {`Beginner To Composer In 14 Days is delightfully different. Moving swiftly from theory to action, Ben emerges as the teacher you always wished you'd had, championing radical creative freedom, improvisation and composition - even for beginners. Especially for beginners in fact. "Students need freedom to truly fall in love with their instrument" he writes, "frameworks that pique their curiosity over and over again so that practice becomes play". Whether you're a complete beginner or have a little knowledge up your sleeve, you'll adore this dynamic and intimate guide to learning the piano, peppered with evocative vignettes of a life lived with music at its heart. Best of all, you'll come away with a method you can use time and time again to create your very own music, captured on professional quality lead sheets you can share with other musicians, to bring your work to life. Suitable for adults and a useful resource for teachers. Complements graded and traditional approaches to learning.`}
             </SlideFadeIn>
 
