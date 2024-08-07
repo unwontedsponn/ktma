04499637d0b3914a846cbde56332165f967ccdbd
unwontedsponn <benpaulspooner@gmail.com>
2024-08-07 14:55:38 +0100

    Adapted the cart and contact modal design

--- app/Cart.tsx
+++ app/Cart.tsx
@@ -16,7 +16,7 @@ const Cart: React.FC<CartProps> = ({ showCartModal, setShowCartModal }) => {
       className={`fixed z-10 left-0 top-0 w-full h-full bg-black bg-opacity-40 ${showCartModal ? 'block' : 'hidden'}`} // Modal overlay styles
     >
       <div 
-        className="modal-content bg-white m-auto p-5 border border-gray-200 w-4/5 md:w-1/2 lg:w-1/3 rounded shadow-lg" // Modal content styles
+        className="modal-content bg-white m-auto p-5 border-3 border-thick-border-gray w-4/5 md:w-1/2 lg:w-1/3 shadow-lg" // Modal content styles
       >
         <span 
           className="close text-gray-400 float-right text-3xl font-bold hover:text-black focus:text-black cursor-pointer" // Close button styles
@@ -24,7 +24,9 @@ const Cart: React.FC<CartProps> = ({ showCartModal, setShowCartModal }) => {
         >
           &times;
         </span>
-        <p className="font-gopher-mono">Shopping Cart Items: <span className="font-bold">Beginner To Composer In 14 Days PDF Book x1</span></p>
+        <p className="font-gopher-mono-semi">Shopping Cart Items: </p>
+        <p className="font-gopher-mono"><span>- </span>Beginner To Composer In 14 Days PDF Book <span>£10</span></p>
+        <button className="border-3 border-thick-border-gray py-2 px-4 hover:cursor-pointer hover:opacity-50">BUY</button>
       </div>
     </div>
   );
--- app/Contact.tsx
+++ app/Contact.tsx
@@ -16,7 +16,7 @@ export default function Contact({ showModal, setShowModal }: ContactProps) {
       className={`fixed z-10 left-0 top-0 w-full h-full bg-black bg-opacity-40 ${showModal ? 'block' : 'hidden'}`} // Modal overlay styles
     >
       <div 
-        className="modal-content bg-white m-auto p-5 border border-gray-200 w-4/5 md:w-1/2 lg:w-1/3 rounded shadow-lg" // Modal content styles
+        className="modal-content bg-white m-auto p-5 border-3 border-thick-border-gray w-4/5 md:w-1/2 lg:w-1/3 shadow-lg" // Modal content styles
       >
         <span 
           className="close text-gray-400 float-right text-3xl font-bold hover:text-black focus:text-black cursor-pointer" // Close button styles
