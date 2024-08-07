5e7f67ec55a1a75fe019fc779546ee8eff3b6de4
unwontedsponn <benpaulspooner@gmail.com>
2024-08-07 14:39:29 +0100

    Improved the cart colours for hover

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useGlobalContext } from './contexts/GlobalContext';
 import Link from 'next/link';
 import Contact from './Contact';
+import Cart from './Cart';
 
 const Header: React.FC = () => {
   const { cartCount } = useGlobalContext();
@@ -92,13 +93,14 @@ const Header: React.FC = () => {
                 {/* Shopping Cart Icon */}                
                 <div
                   id='cart'
-                  className="hidden md:block border-l-2 border-custom-border-color pl-6"
+                  className="hidden md:block border-l-2 border-custom-border-color pl-6 hover:cursor-pointer hover:"
+                  onClick={toggleModal}
                 >                                    
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px" fill="#c15564">
+                    <svg className="cart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px" fill="#3f423e">
                       <path d="M7 4h-2c-.55 0-1 .45-1 1s.45 1 1 1h2l1.68 8.59c.09.46.48.79.95.79h7.5c.47 0 .86-.33.95-.79L20 6H8.25L7 4zm0 2h11.24l-1.31 6.5H9.06L7 6zm0 9c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 3c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm10-3c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 3c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"/>
                       {cartCount > 0 && (
                         <>
-                          <circle cx="18" cy="6" r="6" fill="#c15564"/>
+                          <circle className='cart-icon-circle' cx="18" cy="6" r="6" fill="#c15564"/>
                           <text x="18" y="7" fontFamily="Gopher Mono" fontSize="8" fill="white" textAnchor="middle" alignmentBaseline="middle" dominantBaseline="middle">{cartCount}</text>
                         </>
                       )}                   
@@ -119,6 +121,7 @@ const Header: React.FC = () => {
         </div>
       </section>
       <Contact showModal={showModal} setShowModal={setShowModal} />
+      <Cart showModal={showModal} setShowModal={setShowModal} />
     </>
   );
 };
