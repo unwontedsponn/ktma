039b1de4868cd607f7a01db5edf86b7f605ba7e6
unwontedsponn <benpaulspooner@gmail.com>
2024-08-28 17:16:16 +0100

    added console logs to payment-intent post

--- app/api/stripe/payment-intent/post/route.ts
+++ app/api/stripe/payment-intent/post/route.ts
@@ -12,6 +12,11 @@ if (!stripeSecretKey) {console.error('Stripe Secret Key is missing');}
 const stripe = new Stripe(stripeSecretKey as string, {apiVersion: '2024-06-20',});
 
 export async function POST(req: NextRequest) {
+
+  console.log('NODE_ENV:', process.env.NODE_ENV);
+  console.log('STRIPE_LIVE_SECRET_KEY:', process.env.STRIPE_LIVE_SECRET_KEY ? 'Loaded' : 'Missing');
+  console.log('STRIPE_TEST_SECRET_KEY:', process.env.STRIPE_TEST_SECRET_KEY ? 'Loaded' : 'Missing');  
+
   try {
     const { amount, userId } = await req.json(); // Ensure you receive the userId here
 
