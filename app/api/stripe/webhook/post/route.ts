5c2cb922063e9c6ba61dd28d402bfb15b7946901
unwontedsponn <benpaulspooner@gmail.com>
2024-08-28 17:08:16 +0100

    amended webhook route back to how it previously had the stripe code

--- app/api/stripe/webhook/post/route.ts
+++ app/api/stripe/webhook/post/route.ts
@@ -6,17 +6,9 @@ const isLiveMode = process.env.NODE_ENV === 'production'; // Or use a custom fla
 const stripeSecretKey = isLiveMode ? process.env.STRIPE_LIVE_SECRET_KEY : process.env.STRIPE_TEST_SECRET_KEY;
 const stripeWebhookSecret = isLiveMode ? process.env.STRIPE_LIVE_WEBHOOK_SECRET : process.env.STRIPE_TEST_WEBHOOK_SECRET;
 
-// Check if keys are undefined and throw an error if so
-if (!stripeSecretKey) {throw new Error('Stripe Secret Key is missing');}
-if (!stripeWebhookSecret) {throw new Error('Stripe Webhook Secret is missing');}
-
-const stripe = new Stripe(stripeSecretKey, { apiVersion: '2024-06-20' });
+const stripe = new Stripe(stripeSecretKey as string, {apiVersion: '2024-06-20',});
 
 export async function POST(req: NextRequest) {
-  // Debug logs
-  console.log('NODE_ENV:', process.env.NODE_ENV); // Check environment value
-  console.log('Using Stripe Secret Key:', stripeSecretKey ? 'Key Loaded' : 'Key Missing'); // Check if keys are loaded
-  
   const sig = req.headers.get('stripe-signature') as string;
   const body = await req.text();
 
