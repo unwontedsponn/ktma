eb98be17061537a16a217a00b95e6af5df136814
unwontedsponn <benpaulspooner@gmail.com>
2024-08-28 16:45:30 +0100

    added console logs and some checks

--- app/api/stripe/payment-intent/post/route.ts
+++ app/api/stripe/payment-intent/post/route.ts
@@ -2,8 +2,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const isLiveMode = process.env.NODE_ENV === 'production'; // Or use a custom flag
-const stripeSecretKey = isLiveMode ? process.env.STRIPE_LIVE_SECRET_KEY : process.env.STRIPE_TEST_SECRET_KEY;
+const isLiveMode = process.env.NODE_ENV === 'production';
+const stripeSecretKey = isLiveMode 
+  ? process.env.STRIPE_LIVE_SECRET_KEY 
+  : process.env.STRIPE_TEST_SECRET_KEY;
+
+  if (!stripeSecretKey) {console.error('Stripe Secret Key is missing');}
 
 const stripe = new Stripe(stripeSecretKey as string, {apiVersion: '2024-06-20',});
 
