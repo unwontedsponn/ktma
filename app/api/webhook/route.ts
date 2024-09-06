8c68378f95dd720880977a2de8132287d14b7dbf
unwontedsponn <benpaulspooner@gmail.com>
2024-09-06 14:43:02 +0100

    Updated URL in webhook to my url

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: NextRequest) {
 
       if (userId && email) {
         // Process order
-        const response = await fetch(`http://localhost:3000/api/make-order`, {
+        const response = await fetch(`http://benspooner.co.uk/api/make-order`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ userId, email }),
