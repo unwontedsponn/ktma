b5606b6d18d2a97bbac3f02008d0b682c15c98fa
Ben Spooner <benpaulspooner@gmail.com>
2024-11-02 09:34:23 +0000

    Only shows Footer on devices with md width or larger...

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './sections/Header';
 import Homepage from './sections/Homepage';
 import AboutMe from './sections/AboutMe';
@@ -10,23 +10,39 @@ import { GlobalProvider } from './contexts/GlobalContext';
 
 const Home: React.FC = () => {
   const [showFooter, setShowFooter] = useState(true);
+  const [isSmallViewport, setIsSmallViewport] = useState(false);
+
+  useEffect(() => {
+    const checkViewportWidth = () => {
+      setIsSmallViewport(window.innerWidth < 768); // `md` breakpoint in Tailwind is typically 768px
+    };
+
+    // Check initial width
+    checkViewportWidth();
+
+    // Add resize event listener
+    window.addEventListener('resize', checkViewportWidth);
+
+    // Clean up event listener on component unmount
+    return () => window.removeEventListener('resize', checkViewportWidth);
+  }, []);
 
   const handleGamePlayChange = (playing: boolean) => {
-    // Conditionally hide footer and header when game is actively playing on a small viewport
-    setShowFooter(!playing);
+    // Conditionally hide footer based on playing state and viewport width
+    setShowFooter(!playing && !isSmallViewport);
   };
 
   return (
     <GlobalProvider>
       <main className="background-light flex justify-center">
         <div className="h-screen overflow-y-auto overflow-x-hidden max-w-screen-2xl scroll-snap-y scroll-snap-mandatory">
-          {/* {showHeader && <Header />} */}
           <Header />
           <Homepage />
           <AboutMe />
           <MyBook />
           <MyGame onPlayChange={handleGamePlayChange} />
-          {showFooter && <Footer />}
+          {/* Only show footer if `showFooter` is true and viewport is `md` or larger */}
+          {showFooter && !isSmallViewport && <Footer />}
         </div>
       </main>
     </GlobalProvider>
